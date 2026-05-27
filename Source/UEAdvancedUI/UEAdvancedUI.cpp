04a1b9013ca6d6d8e944ba619925023187ce7f15
v_chchsli <v_chchsli@tencent.com>
2026-05-27 17:42:41 +0800

    Initial commit

---
+++ Source/UEAdvancedUI/UEAdvancedUI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEAdvancedUI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEAdvancedUI, "UEAdvancedUI" );
