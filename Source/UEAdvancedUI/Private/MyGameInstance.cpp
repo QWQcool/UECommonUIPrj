1dda652907cd13f411213d7bf5af21418ecf4abc
v_chchsli <v_chchsli@tencent.com>
2026-06-02 10:32:49 +0800

    feat: 集成Puerts插件并添加TypeScript支持\n\n- 添加完整的Puerts插件（包括核心、编辑器、WasmCore模块）\n- 新增TypeScript配置文件和TypeScript目录\n- 添加MyGameInstance C++类\n- 更新项目构建配置和引擎设置

---
+++ Source/UEAdvancedUI/Private/MyGameInstance.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+void UMyGameInstance::OnStart()
+{
+    Super::OnStart();
+    GameScript = MakeShared<puerts::FJsEnv>();
+    GameScript->Start("MainGame");
+}
+
+void UMyGameInstance::Shutdown()
+{
+    Super::Shutdown();
+    GameScript.Reset();
+}
--- Source/UEAdvancedUI/UEAdvancedUI.Build.cs
+++ Source/UEAdvancedUI/UEAdvancedUI.Build.cs
@@ -8,7 +8,7 @@ public class UEAdvancedUI : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "JsEnv", "Puerts" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
