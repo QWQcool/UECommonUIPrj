5776806513accb0ed89ddf8e012051a5654dcddd
v_chchsli <v_chchsli@tencent.com>
2026-06-03 14:27:04 +0800

    feat: 新增 AsyncAction_PushSoftWidget 蓝图异步操作节点，支持异步加载并推送Widget到堆栈

---
+++ Source/UEAdvancedUI/Public/AsyncActions/AsyncAction_PushSoftWidget.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagContainer.h"
+#include "AsyncAction_PushSoftWidget.generated.h"
+
+class UWidget_ActivatableBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPushSoftWidgetDelegate,UWidget_ActivatableBase*,PushedWidget);
+
+
+/**
+ * 
+ */
+UCLASS()
+class UEADVANCEDUI_API UAsyncAction_PushSoftWidget : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject",HidePin = "WorldContextObject",BlueprintInternalUseOnly = "true",DisplayName = "PushSoftWidget To Widget Stack"))
+	UAsyncAction_PushSoftWidget* PushSoftWidget(
+		const UObject* WorldContextObject,
+		APlayerController *OwningPlayerController,
+		TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
+		UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag,
+		bool bFocusOnNewlyPushedWidget = true
+		);
+
+	//~ Begin UBlueprintAsyncActionBase Interface
+	virtual void Activate() override;
+	//~ End UBlueprintAsyncActionBase Interface
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate OnWidgetCreatedBeforePush;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate AfterPush;
+
+private:
+	TWeakObjectPtr<UWorld> CachedOwningWorld;
+	TWeakObjectPtr<APlayerController> CachedOwningPC;
+	TSoftClassPtr<UWidget_ActivatableBase> CachedSoftWidgetClass;
+	FGameplayTag CachedWidgetStackTag;
+	bool bCachedFocusOnNewlyPushedWidget = false;
+};
