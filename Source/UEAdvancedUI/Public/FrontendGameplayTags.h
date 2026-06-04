2ad3f348a25c2100eef45628be03bf1470b9fe8f
v_chchsli <v_chchsli@tencent.com>
2026-06-04 14:44:12 +0800

    feat: 新增 Frontend GameplayTags 声明
    - 新增 Widget 相关标签：PressAnyKeyScreen、MainMenuScreen、OptionsScreen、ConfirmScreen、KeyRemapScreen
    - 新增 Image 相关标签：TestImage
    - 为所有标签声明添加分号结尾，规范代码格式

--- Source/UEAdvancedUI/Public/FrontendGameplayTags.h
+++ Source/UEAdvancedUI/Public/FrontendGameplayTags.h
@@ -6,9 +6,21 @@
 
 namespace FrontendGameplayTags
 {
-	// Declare gameplay tags (extern declarations)
-	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal)
-	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu)
-	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud)
-	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend)
+	//Frontend widget stack
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+
+	//Frontend widgets
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Widget_PressAnyKeyScreen);
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Widget_MainMenuScreen);
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Widget_OptionsScreen);
+
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Widget_ConfirmScreen);
+
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Widget_KeyRemapScreen);
+
+	//Frontend Options Image
+	UEADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Image_TestImage);
 }
